df9fa7a76e04ab206b1a8433f61abe0b4ad4bfdb
Maghfoor Ahmad <145108970+maghfoormalkana@users.noreply.github.com>
2026-06-02 11:07:29 +0530

    Add Vercel monorepo config

--- server/server.js
+++ server/server.js
@@ -31,6 +31,8 @@ app.use(cors({
   origin: true,
   credentials: true
 }))
+
+
 app.use(morgan('dev'))
 app.use(express.json({ limit: '10mb' }))
 app.use(express.urlencoded({ extended: true, limit: '10mb' }))
